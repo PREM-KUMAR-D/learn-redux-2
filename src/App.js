fd8f309ac8284d40b50bf6c814cdfa21074a1a29
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-12-13 13:32:58 +0530

    inital commit given by instructor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
-
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
 
 function App() {
   return (
-    <div className="App">
-      Redux
-    </div>
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
   );
 }
 
