98090644b4d0ca57ee7c73be31ed2abeba28c5b7
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-12-16 04:48:24 +0530

    added notification component and thunk for sending cart data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { Fragment, useEffect } from 'react';
+import Notification from './components/UI/Notification';
+import { sendCartData } from './store/cart-slice';
+
+let isInitial = true;
 
 function App() {
-  const isCartIsVisible=  useSelector((state)=>state.ui.cartIsVisible);
+  const isCartIsVisible = useSelector((state) => state.ui.cartIsVisible);
 
+  const cart = useSelector(state => state.cart);
+  const notification = useSelector(state=>state.ui.notification);
   
+  const dispatchFunc = useDispatch();
+
+  useEffect(() => {
+
+
+    if(isInitial){
+      isInitial=false;
+      return;
+    }
+    dispatchFunc(sendCartData(cart));
+
+  }, [cart,dispatchFunc])
+
+
 
   return (
+    <Fragment>
+      {notification && <Notification
+        status={notification.status}
+        title={notification.title}
+        message={notification.message}
+      />}
     <Layout>
       {isCartIsVisible && <Cart />}
       <Products />
     </Layout>
+    </Fragment>
   );
 }
 
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,37 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
-    name:'cart',
-    initialState:{
-        items:[],
+    name: 'cart',
+    initialState: {
+        items: [],
         totalQuantity: 0,
     },
-    reducers:{
-        addItemToCart(state,action){
+    reducers: {
+        addItemToCart(state, action) {
             const newItem = action.payload;
-            const existingItem = state.items.find((item)=>item.id === newItem.id);
+            const existingItem = state.items.find((item) => item.id === newItem.id);
             state.totalQuantity++;
-            if(!existingItem){
+            if (!existingItem) {
                 state.items.push({
-                    id:  newItem.id,
+                    id: newItem.id,
                     price: newItem.price,
                     quantity: 1,
                     totalPrice: newItem.price,
                     name: newItem.title,
                 });
-            }else{
+            } else {
                 existingItem.quantity++;
-                existingItem.totalPrice+=newItem.price;
+                existingItem.totalPrice += newItem.price;
             }
         },
-        removeItemFromCart(state,action){
+        removeItemFromCart(state, action) {
             const id = action.payload;
-            const existingItem = state.items.find((item)=> item.id === id);
+            const existingItem = state.items.find((item) => item.id === id);
             state.totalQuantity--;
-            if(existingItem.quantity === 1){
-                state.items = state.items.filter((item)=> item.id !== id);
+            if (existingItem.quantity === 1) {
+                state.items = state.items.filter((item) => item.id !== id);
             }
-            else{
+            else {
                 existingItem.quantity--;
                 existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
             }
@@ -40,6 +41,45 @@ const cartSlice = createSlice({
 
     }
 })
+
+export const sendCartData = (cart) => {
+    return async (dispatch) => {
+
+        dispatch(uiActions.showNotification({
+            status: 'pending',
+            title: 'sending..',
+            message: 'sending cart data',
+        }))
+        const sendRequest = async () => {
+
+            const fireBaseUrl = process.env.REACT_APP_FIRE_BASE_URL;
+            const response = await fetch(`${fireBaseUrl}/cart-redux.json`, {
+                method: 'PUT',
+                body: JSON.stringify(cart),
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            })
+            if (!response.ok) {
+                throw new Error('Sending cart data failed.')
+            }
+        }
+        try{
+
+            await sendRequest();
+        }
+        catch(error){
+
+            dispatch(uiActions.showNotification({
+                status: 'success',
+                title: 'success..',
+                message: 'sent cart data succefully',
+            }))
+        }
+    }
+}
+
+
 
 export const cartActions = cartSlice.actions;
 
